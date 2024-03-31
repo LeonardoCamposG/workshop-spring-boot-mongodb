651c9ff6d7094309df4b0cb1ea9d2e4aecc0f377
Leonardo Campos Guimarães <contato.leonardocg@gmail.com>
2024-03-31 09:23:44 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/leocg/workshopmongo/config/Instantion.java
+++ src/main/java/com/leocg/workshopmongo/config/Instantion.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.leocg.workshopmongo.domain.Post;
 import com.leocg.workshopmongo.domain.User;
+import com.leocg.workshopmongo.dto.AuthorDTO;
 import com.leocg.workshopmongo.repository.PostRepository;
 import com.leocg.workshopmongo.repository.UserRepository;
 
@@ -35,10 +36,12 @@ public class Instantion implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo, Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom Dia", "Acordei feliz hoje!", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo, Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom Dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 }
